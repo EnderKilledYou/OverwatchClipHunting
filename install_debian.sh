a691603dc7c1a285c8f1bec98b5c919190b48c89
Ender <sterlingkooshesh@gmail.com>
2022-08-15 15:54:28 -0500

    commit and push

--- install_debian.sh
+++ install_debian.sh
@@ -4,3 +4,8 @@
   apt install python3 python3-pip -y
   apt-get install ffmpeg libsm6 libxext6  -y
   apt-get install -y cron
+  apt-get install -y ca-certificates
+  sed -i '/^mozilla\/DST_Root_CA_X3.crt$/ s/^/!/' /etc/ca-certificates.conf
+  update-ca-certificates
+  apt-get update
+  apt-get -y upgrade
--- startup.sh
+++ startup.sh
@@ -2,4 +2,3 @@ chmod 0644 /etc/cron.d/backup-cron
 crontab /etc/cron.d/backup-cron
 /etc/init.d/cron start
 python3 startup_file.py
-update-ca-certificates -f
