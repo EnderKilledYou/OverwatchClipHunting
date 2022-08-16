b8ca31710da5e5ee486c1f208e4c0ea6a9ac0005
Ender <sterlingkooshesh@gmail.com>
2022-08-16 17:57:09 -0500

    commit and push

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,6 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --quiet --no-cache-dir -r Requirements.txt
 rm -rf static
-unzip front_end.zip static
-rm -rf front_end front_end.zip
+unzip /app/front_end.zip -d /app/static
+rm -rf /app/front_end /app/front_end.zip
 
