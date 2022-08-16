f2d790f48030fbbe7d8201c1f5b135923c846893
Ender <sterlingkooshesh@gmail.com>
2022-08-15 23:36:24 -0500

    commit and push

---
+++ front_end/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <a class="navbar-brand" href="#/">Clip Hunta</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+
+          <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+        </li>
+
+        <li class="nav-item">
+          <router-link class="nav-link" to="/clips_viewer">clips viewer</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/search_twitch/">search</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/list_scan_jobs/">scanners</router-link>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+
+
+  <router-view/>
+</template>
---
+++ front_end/src/views/HomeView.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="home">
+    <label for="streamer_name"> Add Streamer
+      <input class="text-info" id="streamer_name" type="text" v-model="streamerName">
+      <button @click="Watch()" class="btn btn-block">Add</button>
+      <button @click="Twitch()" class="btn btn-block" v-show="twitch_streams_filtered.length ===0">Look at Twitch
+      </button>
+      <button @click="HideTwitch()" class="btn btn-block" v-show="twitch_streams_filtered.length !==0">Hide</button>
+      <button @click="AutoTwitch()" class="btn btn-block">Auto Twitch</button>
+      <button @click="StopAutoTwitch()" class="btn btn-block">Stop Auto Twitch</button>
+    </label>
+    <table class="table table-striped table-responsive">
+      <thead>
+      <tr>
+        <th>
+          Started At
+        </th>
+        <th>
+
+        </th>
+
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="watcher in twitch_streams_filtered">
+        <td>
+          <figure class="figure">
+            <a target="_blank" :href="`https://twitch.tv/` +watcher.video_id"> <img
+                :src="watcher.thumbnail_url.replace('{width}','100').replace('{height}','100')"
+                class="  img-responsive"/></a>
+
+            <figcaption> {{ watcher.user_name }} - {{ watcher.title }} - {{ watcher.viewer_count }}</figcaption>
+            <figcaption> {{ watcher.started_at }}</figcaption>
+          </figure>
+        </td>
+        <td>
+          <button class="btn btn-danger" @click="Watch2( watcher.user_name)">Watch</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <table class="table table-striped table-responsive">
+      <thead>
+      <tr>
+        <th>
+          Streamer Name
+        </th>
+        <th>
+          Backqueue in seconds
+        </th>
+        <th>
+          Backqueue in frames
+        </th>
+        <th>
+          Frames Read
+
+        </th>
+        <th>
+          Frames Done
+        </th>
+        <th>
+
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="watcher in items">
+        <td>
+          {{ watcher.name }}
+        </td>
+
+        <td>
+          {{ watcher.seconds }}
+        </td>
+        <td>
+          {{ watcher.queue_size }}
+        </td>
+        <td>
+          {{ watcher.frames_read }}
+
+        </td>
+        <td>
+          {{ watcher.frames_done }}
+        </td>
+        <td>
+          <button class="btn btn-danger" @click="Unwatch(watcher)">Stop</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue';
+import {add_streamer, list_streamer, remove_streamer, StreamerMonitorState, API} from "@/api"; // @ is an alias to /src
+
+let interval: number | null | undefined = null
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {
+  private items: StreamerMonitorState[] = [];
+  streamerName: string = ""
+  private interval: number = 0;
+  private twitch_streams: any[] = [];
+
+  async HideTwitch() {
+    this.twitch_streams = []
+  }
+
+  async Twitch() {
+    this.twitch_streams = await API.get_live_streamers()
+  }
+
+  async AutoTwitch() {
+    await API.start_farm_twitch()
+
+  }
+
+  async StopAutoTwitch() {
+    await API.stop_farm_twitch()
+
+  }
+
+  get twitch_streams_filtered() {
+    if (!this.twitch_streams) return []
+    return this.twitch_streams.filter(a => {
+      return !this.items.find(b => b.name.toLowerCase() === a.user_name.toLowerCase())
+    })
+  }
+
+  created() {
+    this.list_items()
+    if (interval)
+      clearInterval(interval)
+    interval = setInterval(this.list_items.bind(this), 5000)
+  }
+
+
+  async Watch2(streamerName: string) {
+    const streamerResponse = await API.add(streamerName)
+    this.items = streamerResponse.items
+  }
+
+  async Watch() {
+    const streamerResponse = await API.add(this.streamerName)
+    this.items = streamerResponse.items
+  }
+
+  async Unwatch(watcher: StreamerMonitorState) {
+    const streamerResponse = await API.remove(watcher.name)
+
+    this.items = streamerResponse.items
+  }
+
+  async list_items() {
+
+    const streamerResponse = await API.list()
+    this.items = streamerResponse.items
+  }
+
+}
+</script>
