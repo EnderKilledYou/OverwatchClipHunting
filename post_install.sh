1b5dbefe78969e74690cb7440cadd2175a64bcc6
Ender <sterlingkooshesh@gmail.com>
2022-08-16 15:24:51 -0500

    commit and push

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,7 @@
 python3 -m pip install --quiet --no-cache-dir -r Requirements.txt
 cd $VUE_HOME
 npm install  && npm run build
+rm -rf node_modules
 mkdir $TESSERACT_DATA_FAST_INSTALL_FOLDER
 cd $TESSERACT_DATA_FAST_INSTALL_FOLDER
 git clone https://github.com/tesseract-ocr/tessdata_fast.git < /dev/null > /dev/null
