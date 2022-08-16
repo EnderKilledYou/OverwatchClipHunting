077869f700706b2a794a3e3e2528b215048f1ffd
Ender <sterlingkooshesh@gmail.com>
2022-08-15 23:08:08 -0500

    commit and push

--- install_debian.sh
+++ install_debian.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-  apt update -y
-  apt upgrade -y
-  apt install python3 python3-pip -y
-  apt-get install ffmpeg libsm6 libxext6  -y
-  apt-get install -y cron
-  apt-get install -y ca-certificates
-  sed -i '/^mozilla\/DST_Root_CA_X3.crt$/ s/^/!/' /etc/ca-certificates.conf
-  update-ca-certificates
-  apt-get update
-  apt-get -y upgrade
-  curl -sL https://deb.nodesource.com/setup_16.x | bash -
-  apt -y install nodejs
-  node  -v
-  cd /app/front_end/
-  npm install && npm run build
+apt update -y
+apt upgrade -y
+apt install python3 python3-pip -y
+apt-get install ffmpeg libsm6 libxext6 -y
+apt-get install -y cron
+apt-get install -y ca-certificates
+sed -i '/^mozilla\/DST_Root_CA_X3.crt$/ s/^/!/' /etc/ca-certificates.conf
+update-ca-certificates
+apt-get update
+apt-get -y upgrade
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+bash /tmp/nodesource_setup.sh
+apt-get update
+apt-get -y upgrade
+apt install nodejs -y
+node -v
+npm -v
--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 chmod 0644 /etc/cron.d/backup-cron
 crontab /etc/cron.d/backup-cron
 /etc/init.d/cron start
+
+cd /app/front_end/
+npm install && npm run build
 python3.8 startup_file.py
