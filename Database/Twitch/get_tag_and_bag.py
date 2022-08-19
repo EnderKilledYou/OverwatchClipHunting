88e5450bc64396a0f369dae30b266ed076514960
Ender <sterlingkooshesh@gmail.com>
2022-08-19 16:29:49 -0500

    commit and push

--- Database/Twitch/get_tag_and_bag.py
+++ Database/Twitch/get_tag_and_bag.py
@@ -1,12 +1,16 @@
 from typing import List
 
+from Database.Twitch.dict_to_class import Dict2Class
 from Database.Twitch.twitch_clip_tag import TwitchClipTag
 from config.db_config import db
 
 
 def get_tag_and_bag_by_clip_id(clip_id: int) -> List[TwitchClipTag]:
     with db.session.begin():
-        items = list(TwitchClipTag.query.filter_by(clip_id=clip_id))
-    for item in items:
-        db.session.expunge(item)
+        items = list(list_dicts(map(TwitchClipTag.query.filter_by(clip_id=clip_id))))
+
     return items
+
+
+def list_dicts(items):
+    return map(lambda x: Dict2Class(x), items)
