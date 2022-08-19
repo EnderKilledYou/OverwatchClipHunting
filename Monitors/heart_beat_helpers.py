a95f33fd6d446cda211b71d425741ba58b6264ff
Ender <sterlingkooshesh@gmail.com>
2022-08-19 16:03:53 -0500

    commit and push

--- Monitors/heart_beat_helpers.py
+++ Monitors/heart_beat_helpers.py
@@ -1,16 +1,16 @@
 import os
 from typing import List
 
+import cloud_logger
 from Database.live_twitch_instance import LiveTwitchInstance
-from Database.monitor import get_claimed_count, claim_monitor, get_monitor_by_name, reset_for_claim
-from cloud_logger import cloud_logger, cloud_message
+from Database.monitor import claim_monitor, get_monitor_by_name, reset_for_claim
 
 
-def claim_one_monitor( streams: List[LiveTwitchInstance],claimed_count:int):
-    cloud_logger()
+def claim_one_monitor(streams: List[LiveTwitchInstance], claimed_count: int):
+    cloud_logger.cloud_logger()
 
     if claimed_count >= max_active_monitors:
-        cloud_message("No space to start new streamers")
+        cloud_logger.cloud_message("No space to start new streamers")
         return
 
     for stream in streams:
@@ -18,6 +18,7 @@ def claim_one_monitor( streams: List[LiveTwitchInstance],claimed_count:int):
         claimed = claim_monitor(user_login)
         if not claimed:
             continue
+        cloud_logger.cloud_message("Claimed One" + user_login)
         monitor = get_monitor_by_name(user_login)
         reset_for_claim(user_login)
 
