3b91e206d22049f4e23c1007c7d8efeda71428ed
Ender <sterlingkooshesh@gmail.com>
2022-08-19 04:32:07 -0500

    commit and push

--- cloud_logger.py
+++ cloud_logger.py
@@ -10,6 +10,14 @@ from twitchAPI import Twitch
 
 should_log = "CLOUD_PRINT" in os.environ
 
+class_checker = lambda obj: isinstance(obj, Twitch)
+class_checker_bytes = lambda obj: isinstance(obj, bytes)
+class_checker_default = lambda obj: isinstance(obj, object)
+# then assign it to a function that does the converting
+json.override_table[class_checker] = lambda obj_of_that_class: "Twitch Api"
+json.override_table[class_checker_bytes] = lambda obj_of_that_class: str(obj_of_that_class, "utf-8")
+json.override_table[class_checker_default] = lambda obj_of_that_class: obj_of_that_class.to_dict() if hasattr(obj_of_that_class,'to_dict') else str(obj_of_that_class)
+
 
 def cloud_error_logger(e, file=sys.stderr):
     args = '<none>'
