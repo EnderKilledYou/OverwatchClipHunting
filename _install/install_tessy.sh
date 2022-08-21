ab796b3b9812ba2df621a9d93e63a0ebe5719e6f
Ender <sterlingkooshesh@gmail.com>
2022-08-21 12:29:43 -0500

    commit and push

--- _install/install_tessy.sh
+++ _install/install_tessy.sh
@@ -1,3 +1,5 @@
+export TESSERACT_DATA_FAST_INSTALL_FOLDER=/FAST_DATA
+export TESSERACT_DATA_FAST=/FAST_DATA/tessdata_fast/
 mkdir $TESSERACT_DATA_FAST_INSTALL_FOLDER
 cd $TESSERACT_DATA_FAST_INSTALL_FOLDER
 git clone https://github.com/tesseract-ocr/tessdata_fast.git
--- run.bash
+++ run.bash
@@ -1,2 +1,4 @@
+export TESSERACT_DATA_FAST_INSTALL_FOLDER=/FAST_DATA
+export TESSERACT_DATA_FAST=/FAST_DATA/tessdata_fast/
 git pull
 sudo gunicorn --bind :80 --workers 1 --threads 8 --timeout 0 app:app  > log.txt 2>&1
