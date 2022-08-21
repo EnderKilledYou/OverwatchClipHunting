35d91c69b2e0bee71605e5883d0a01f19733eabf
Ender <sterlingkooshesh@gmail.com>
2022-08-21 12:24:27 -0500

    commit and push

--- Database/Twitch/dict_to_class.py
+++ Database/Twitch/dict_to_class.py
@@ -7,11 +7,18 @@ class Dict2Class(object):
     def __getattribute__(self, attr):
         return object.__getattribute__(self, attr)
 
+    def __iter__(self):
+        iters = dict((x, y) for x, y in Dict2Class.__dict__.items() if x[:2] != '__')
+
+        iters.update(self.__dict__)
+
+        for x, y in iters.items():
+            yield x, y
 
     def __init__(self, my_dict):
         self._dict = my_dict
         for key in my_dict:
-            if isinstance(my_dict[key],bytes):
+            if isinstance(my_dict[key], bytes):
                 str(my_dict[key], "utf-8")
             else:
                 setattr(self, key, my_dict[key])
