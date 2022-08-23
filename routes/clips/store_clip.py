17351f7f56da0fbda8d4f3073b2a9f7261db900b
Ender <sterlingkooshesh@gmail.com>
2022-08-23 01:44:42 -0500

    commit and push

--- routes/clips/store_clip.py
+++ routes/clips/store_clip.py
@@ -6,6 +6,7 @@ from Database.Twitch.twitch_clip_instance import add_twitch_clip_instance_from_a
 from Database.Twitch.twitch_clip_instance_scan_job import add_twitch_clip_scan
 from Events.flask_events import flask_event
 from cloud_logger import cloud_logger, cloud_error_logger
+from start_up_flask import rescanner
 from twitch_helpers.twitch_helpers import get_twitch_api
 
 
@@ -25,8 +26,8 @@ def store_clip(clip_data, type):
         (clip_id, clip_broadcaster) = add_twitch_clip_instance_from_api(clip['data'][0], type)
         job_id = add_twitch_clip_scan(clip_id, clip_broadcaster)
 
-        # if job_id is not None:
-        #     rescanner.add_job(job_id)
+        if job_id is not None:
+            rescanner.add_job(job_id)
     except BaseException as e:
         cloud_error_logger(e, file=sys.stderr)
         traceback.print_exc()
