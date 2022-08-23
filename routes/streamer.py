c3610bc3eab704f283d4c816e3ae26c6743b0147
Ender <sterlingkooshesh@gmail.com>
2022-08-23 12:35:57 -0500

    commit and push

--- routes/streamer.py
+++ routes/streamer.py
@@ -2,6 +2,7 @@ import json_fix
 from flask import Blueprint
 from Database.monitor import remove_stream_to_monitor, add_stream_to_monitor, get_all_my_monitors, get_all_monitors
 from cloud_logger import cloud_error_logger
+from routes.route_cache import cache
 from start_up_flask import alli
 from twitch_helpers.get_monitored_streams import get_monitored_streams
 from twitch_helpers.twitch_helpers import get_twitch_api
@@ -21,15 +22,18 @@ def add(stream_name: str):
 @sharp.function()
 def list_streamers():
     try:
-        my_monitors = get_all_monitors()
+        my_monitors = cache.get('my_monitors')
+        if my_monitors is None:
+            my_monitors = get_all_monitors()
+            cache.set('my_monitors', 120)
         if len(my_monitors) == 0:
             return {"success": True, 'items': []}
 
         twitch_api = get_twitch_api()
-        streams = None  # cache.get('get_monitored_streams')
+        streams = cache.get('get_monitored_streams')
         if streams is None:
             streams = get_monitored_streams(twitch_api)
-            # cache.set('get_monitored_streams', 30)
+            cache.set('get_monitored_streams', 120)
         dictsm = list_obj_to_list_dicts(my_monitors)
         dictss = list_obj_to_list_dicts(streams)
         return {"success": True, 'items': [dictsm, dictss]}
