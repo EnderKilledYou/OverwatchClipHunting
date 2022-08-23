fda948bc36ffc24b2ab6054f9c4e6c86695eddd8
Ender <sterlingkooshesh@gmail.com>
2022-08-23 08:17:40 -0500

    commit and push

--- Ocr/vod_downloader.py
+++ Ocr/vod_downloader.py
@@ -7,11 +7,11 @@ from twitchdl.commands.download import get_clip_authenticated_url
 from Ocr.twitch_dl_args import Args
 
 
-def fast_download_file(url,local_filename):
-
+def fast_download_file(url, local_filename):
     with requests.get(url, stream=True) as r:
         with open(local_filename, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
+            r.close()
 
     return local_filename
 
@@ -28,8 +28,10 @@ def _download_clip(slug, args):
 
     print("Downloading clip...")
     print(url, args.output)
-    fast_download_file(url,args.output)
+    fast_download_file(url, args.output)
     print("Downloaded: {} ".format(args.output))
 
-if __name__ =='__main__':
-    _download_clip('KathishIncredulousMilkCharlietheUnicorn-Z2f3jnIXtOkmAwwZ',Args('KathishIncredulousMilkCharlietheUnicorn-Z2f3jnIXtOkmAwwZ','test.mp4'))
+
+if __name__ == '__main__':
+    _download_clip('KathishIncredulousMilkCharlietheUnicorn-Z2f3jnIXtOkmAwwZ',
+                   Args('KathishIncredulousMilkCharlietheUnicorn-Z2f3jnIXtOkmAwwZ', 'test.mp4'))
