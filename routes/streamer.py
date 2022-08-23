4bf8c674fd462cc727ec087105b4f7229ebecbce
Ender <sterlingkooshesh@gmail.com>
2022-08-23 14:31:10 -0500

    commit and push

--- routes/streamer.py
+++ routes/streamer.py
@@ -30,7 +30,7 @@ def list_streamers():
         else:
             my_monitors = get_all_monitors_dicts()
             cache.set('my_monitors', my_monitors,120)
-            my_monitors = my_monitors
+
         if len(my_monitors) == 0:
             return {"success": True, 'items': []}
 
