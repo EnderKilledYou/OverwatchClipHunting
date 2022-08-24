cf491268889003be6fb405b7932bae48d00a0118
Ender <sterlingkooshesh@gmail.com>
2022-08-24 14:30:39 -0500

    commit and push

--- Ocr/VideoCapReader.py
+++ Ocr/VideoCapReader.py
@@ -144,7 +144,7 @@ class VideoCapReader:
             frame_number = frame_number + 1
 
     def _acquire(self, url: str):
-        self.video_capture = cv2.VideoCapture(url)
+        self.video_capture = cv2.VideoCapture(url, apiPreference=cv2.CAP_IMAGES)
         self.video_capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
         if not self.video_capture:
             raise NoStreamError("Capture could not open stream")
@@ -156,8 +156,6 @@ class VideoCapReader:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._release()
-
-
 
     def _release(self):
         if self.video_capture is None:
