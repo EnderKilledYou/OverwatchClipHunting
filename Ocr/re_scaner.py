41b1c57119b305f876a776dbcbccc16554ca9af1
Ender <sterlingkooshesh@gmail.com>
2022-08-24 04:27:16 -0500

    commit and push

--- Ocr/re_scaner.py
+++ Ocr/re_scaner.py
@@ -50,7 +50,7 @@ class ReScanner(ThreadedManager):
         self._reader = None
         self._frame_count = 0
         self.matcher = OverwatchClipReader()
-    
+
     def _do_work(self, job_id: int):
         cloud_logger()
         wait_for_tesseract()
@@ -69,28 +69,29 @@ class ReScanner(ThreadedManager):
 
             update_twitch_clip_instance_filename(job.clip_id, path)
             update_scan_job_in_scanning(job.id)
-            self._scan_clip(job, path)
+            self._scan_clip(job_id, job.broadcaster, job.clip_id, path)
 
             update_scan_job_in_deepfacequeue(job.id)
-            Timer(0, face_to_clip, (job.clip_id, path, job.id)).start()
-
+            # Timer(0, face_to_clip, (job.clip_id, path, job.id)).start()
+            update_scan_job_percent(job.id, 1, True)
+            return
 
         except BaseException as e:
             cloud_error_logger(e, file=sys.stderr)
             traceback.print_exc()
             if job is not None:
                 update_scan_job_error(job.id, str(e))
-    
+
     def _get_url(self, job: TwitchClipInstanceScanJob):
         video_id = get_twitch_clip_video_id_by_id(job.clip_id)
         if video_id is None:
             return None
         return video_id
-    
+
     def _stop(self):
         if self._reader is not None:
             self._reader.stop()
-    
+
     def match_frame(self, itr, api):
 
         try:
@@ -103,9 +104,9 @@ class ReScanner(ThreadedManager):
         except BaseException as b:
             return False
         return True
-    
-    def _scan_clip(self, job: TwitchClipInstanceScanJob, path: str):
-        with ClipVideoCapReader(job.broadcaster, job.clip_id) as reader:
+
+    def _scan_clip(self, job_id: int, broadcaster: str, clip_id: int, path: str):
+        with ClipVideoCapReader(broadcaster, clip_id) as reader:
             # size = len(reader_list)
             frame_number = 0
             seconds = get_length(path)
@@ -117,22 +118,22 @@ class ReScanner(ThreadedManager):
                     while self.match_frame(itr, api):
                         self._frame_count += 1
                         frame_number = frame_number + 1
-                        self._update_percentage_in_row(frame_number, job, size)
+                        self._update_percentage_in_row(frame_number, job_id, size)
 
             except BaseException as b:
                 traceback.print_exc()
                 pass
 
-        update_scan_job_percent(job.id, 1)
-    
-    def _update_percentage_in_row(self, frame_number, job, size):
+        update_scan_job_percent(job_id, 1)
+
+    def _update_percentage_in_row(self, frame_number, job_id, size):
         if frame_number < size:
             percent_done = frame_number / size
         else:
             percent_done = .99  # we guessed the fps wrong (ffmped)
         i = int(percent_done * 100.0)
         if i > 0 and i % 15 == 0:
-            update_scan_job_percent(job.id, percent_done)
+            update_scan_job_percent(job_id, percent_done)
 
 
 def queue_to_list(queue: Queue):
