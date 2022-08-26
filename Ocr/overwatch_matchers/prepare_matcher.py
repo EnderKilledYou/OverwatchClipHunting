0dcaaa9213dc008d60b75650f78817ffda702e09
Ender <sterlingkooshesh@gmail.com>
2022-08-26 02:43:20 -0500

    commit and push

--- Ocr/overwatch_matchers/prepare_matcher.py
+++ Ocr/overwatch_matchers/prepare_matcher.py
@@ -9,7 +9,7 @@ CONTESTED = re.compile('CONTESTED', re.IGNORECASE)
 
 
 def if_prepare_attack(text: str):
-    words = text.split(' ')
+
     return PREPARE.search(text) is not None and ATTACK.search(text) is not None
 
 
