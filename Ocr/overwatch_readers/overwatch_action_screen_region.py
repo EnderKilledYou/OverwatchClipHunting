2672afb4de5c70239317d8bed169c79cf10923c2
Ender <sterlingkooshesh@gmail.com>
2022-08-26 18:52:23 -0500

    commit and push

--- Ocr/overwatch_readers/overwatch_action_screen_region.py
+++ Ocr/overwatch_readers/overwatch_action_screen_region.py
@@ -40,7 +40,7 @@ class OverwatchActionScreenRegion(ScreenRegion):
         api.SetImage(img)
         text = api.GetUTF8Text()
         conf = api.MeanTextConf()
-        print(conf)
+
         frame.empty = True
         if len(text) < 4:
             return
