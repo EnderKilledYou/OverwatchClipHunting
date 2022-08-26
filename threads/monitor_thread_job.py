65e770f254144fa78f9735fbcb4ed255cb710052
Ender <sterlingkooshesh@gmail.com>
2022-08-26 02:53:48 -0500

    commit and push

--- threads/monitor_thread_job.py
+++ threads/monitor_thread_job.py
@@ -1,24 +1,32 @@
+import gc
+
 from Database.monitor import Monitor
 from Database.unclaim_monitor import unclaim_table_type
 from Ocr.twitch_video_frame_buffer import TwitchEater
+from cloud_logger import cloud_error_logger
 from threads.TableThreadJob import TableThreadJob
 
 
 class MonitorThreadJob(TableThreadJob):
 
     def __init__(self, broadcaster):
-        super(MonitorThreadJob,self).__init__(Monitor)
+        super(MonitorThreadJob, self).__init__(Monitor)
         self.broadcaster = broadcaster
 
     def _do_work(self, work: Monitor):
         print(f"starting do broadcast for {work.broadcaster}")
-        with TwitchEater(work.broadcaster) as ocr:
-            self._stop = ocr.stop
-            self._get_stats = ocr.get_stats
-            ocr.buffer_broadcast()
-            self._get_stats = None
-            self._stop = None
-            print(f"Exiting do broadcast for {work.broadcaster}")
+        try:
+            with TwitchEater(work.broadcaster) as ocr:
+                self._stop = ocr.stop
+                self._get_stats = ocr.get_stats
+                ocr.buffer_broadcast()
+                self._get_stats = None
+                self._stop = None
+                print(f"Exiting do broadcast for {work.broadcaster}")
+        except BaseException as b:
+            cloud_error_logger(b)
+
+        gc.collect()
         print(f"Unclaiming for {work.broadcaster}")
         unclaim_table_type(Monitor, self._id)
         print(f"Unclaimed for {work.broadcaster}")
