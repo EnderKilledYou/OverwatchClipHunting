61ff27dae273524373909ae4cf9e1c938f13a587
Ender <sterlingkooshesh@gmail.com>
2022-08-26 04:23:23 -0500

    commit and push

--- Ocr/twitch_video_frame_buffer.py
+++ Ocr/twitch_video_frame_buffer.py
@@ -76,7 +76,7 @@ class TwitchEater(VideoFrameBuffer):
         # count = os.cpu_count()
         # print("Cpu threads would be " + str(count))
         # if count is None:
-        count = 1
+        count = 2
         for i in range(0, count):
             consumer_thread = threading.Thread(target=matcher.consume_twitch_broadcast)
             self.consumer_threads.append(consumer_thread)
