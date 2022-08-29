5d423c7bfecd3fbda491f440d10da9e5f4a5805e
Ender <sterlingkooshesh@gmail.com>
2022-08-29 09:35:40 -0500

    commit and push

--- scanner.py
+++ scanner.py
@@ -2,4 +2,4 @@ from Events.system import system_events
 from Ocr.re_scaner import ReScanner
 
 rescanner = ReScanner()
-system_events.emit('rescan', rescanner.add_job)
+system_events.on('rescan', rescanner.add_job)
