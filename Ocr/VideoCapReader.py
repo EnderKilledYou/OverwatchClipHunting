bf60619c3dee01561f227c2569c7f31ec8202489
Ender <sterlingkooshesh@gmail.com>
2022-08-29 13:37:20 -0500

    commit and push

--- Ocr/VideoCapReader.py
+++ Ocr/VideoCapReader.py
@@ -169,7 +169,7 @@ class VideoCapReader:
 
     def _next_frame2(self, frame_number, buffer: Queue, video_capture):
         if self.count() > 100:
-            #print(f"transfer full, waiting {self.streamer_name}")
+            # print(f"transfer full, waiting {self.streamer_name}")
             sleep(3)
 
         item = self._read_one2(frame_number, self.fps, video_capture)
@@ -177,7 +177,7 @@ class VideoCapReader:
             return True
         should_sleep = False
         if frame_number > 0 and frame_number % 10 == 0 and self.count() == 0:
-            #print(f"Sleeping off empty buffer {self.streamer_name}")
+            # print(f"Sleeping off empty buffer {self.streamer_name}")
             should_sleep = True
 
         buffer.put(item)
@@ -213,7 +213,7 @@ class VideoCapReader:
 
     def _next_frame(self, frame_number, buffer: Queue):
         if self.count() > 100:
-            #print(f"transfer full, waiting {self.streamer_name}")
+            # print(f"transfer full, waiting {self.streamer_name}")
             sleep(3)
 
         item = self._read_one(frame_number, self.fps)
@@ -221,7 +221,7 @@ class VideoCapReader:
             return True
 
         if frame_number > 0 and frame_number % 10 == 0 and self.count() == 0:
-            #print(f"Sleeping off empty buffer {self.streamer_name}")
+            # print(f"Sleeping off empty buffer {self.streamer_name}")
             sleep(1)  # let the video cap have some time to buffer
         buffer.put(item)
 
@@ -242,10 +242,9 @@ class VideoCapReader:
             yield item
             self.incr_items_read()
 
-
-
     def _acquire2(self, url: str):
         video_capture = cv2.VideoCapture(url, apiPreference=cv2.CAP_IMAGES)
+        print("Capture was, setting to 500 " + cv2.get(cv2.CAP_PROP_BUFFERSIZE))
         video_capture.set(cv2.CAP_PROP_BUFFERSIZE, 500)
         if not video_capture:
             raise None
