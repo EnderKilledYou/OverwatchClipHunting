94ebc6f64240251a1ec44a502ec4ba9c65e3f6b0
Ender <sterlingkooshesh@gmail.com>
2022-08-30 10:31:06 -0500

    commit and push

--- Ocr/overwatch_matchers/elimination_matcher.py
+++ Ocr/overwatch_matchers/elimination_matcher.py
@@ -3,5 +3,7 @@ from typing import Optional, Match
 
 
 def if_got_elim(text: str) -> bool:
-    return  'ELMUARTER' in text or 'NATED' in text or 'NATD' in text or 'MNATE' in text or 'MTATD' in text
+    return 'ELMUARTER' in text or 'NATED' in text or 'NATD' in text or 'MNATE' in text or 'MTATD' in text
 
+def if_got_HOOKED(text: str) -> bool:
+    return 'ENEMY HOOKED' in text
