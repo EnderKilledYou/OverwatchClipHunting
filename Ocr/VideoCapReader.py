e8e4416f1d22b0ce7b3f25ca3dffab761644a98e
Ender <sterlingkooshesh@gmail.com>
2022-08-30 16:14:51 -0500

    commit and push

--- Ocr/VideoCapReader.py
+++ Ocr/VideoCapReader.py
@@ -105,7 +105,7 @@ class VideoCapReader:
         print(f"Opening {url} for {self.streamer_name}")
         video_capture = cv2.VideoCapture(url)
 
-        # video_capture.set(cv2.CAP_PROP_BUFFERSIZE, 15)
+        video_capture.set(cv2.CAP_PROP_BUFFERSIZE, 300)
         if not video_capture:
             raise None
         video_capture.open(url)
