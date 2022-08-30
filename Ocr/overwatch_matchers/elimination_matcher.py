caaabdce983e21dbc89f8bac9ba9e1f1a2955794
Ender <sterlingkooshesh@gmail.com>
2022-08-30 13:12:02 -0500

    commit and push

--- Ocr/overwatch_matchers/elimination_matcher.py
+++ Ocr/overwatch_matchers/elimination_matcher.py
@@ -1,9 +1,12 @@
 import re
 from typing import Optional, Match
 
+nated = re.compile('[MI]{1,2}NATE[ED]{1,2}', re.IGNORECASE)
+
 
 def if_got_elim(text: str) -> bool:
-    return 'RTED' in text or 'NATED' in text or 'NATD' in text or 'MNATE' in text or 'MTATD' in text
+    return 'RTED' in text or 'NATED' in text or 'NATD' in text or 'MNATE' in text or 'MTATD' in text or nated.search(
+        text) is not None
 
 
 def if_got_HOOKED(text: str) -> bool:
